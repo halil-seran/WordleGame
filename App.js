64fcd024a623fc47f85d88d6a5dc906c1152cfbc
Halil <halil.seran.1135@gmail.com>
2022-03-16 23:22:43 +0300

    icantsee

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import styled from "styled-components/native";
+import { colors } from "./src/constants";
+import Keyboard from "./src/components/Keyboard/";
+
+const NUMBER_OF_TRIES = 6;
 
 export default function App() {
+  const word = "hello";
+  const letters = word.split(""); // ['h','e','l','l','o']
+
+  const rows = new Array(NUMBER_OF_TRIES).fill(
+    new Array(letters.length).fill("a")
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Screen>
+      <StatusBar style="light" />
+      <HeaderTitle>WORDLE</HeaderTitle>
+      <Map>
+        {rows.map((row) => (
+          <Row>
+            {row.map((cell) => (
+              <Cell>
+                <CellLetter>{cell.toUpperCase()}</CellLetter>
+              </Cell>
+            ))}
+          </Row>
+        ))}
+      </Map>
+      <Keyboard />
+    </Screen>
   );
 }
 
+const Map = styled.View`
+  align-self: stretch;
+  height: 100px;
+`;
+const Row = styled.View`
+  flex-direction: row;
+  align-self: stretch;
+  justify-content: center;
+`;
+const Cell = styled.View`
+  border-width:2px;
+  border-color:white;
+  margin:5px;
+  height:30px;
+  aspect-ratio:1;
+  flex:1
+  max-width:70px;
+  align-items: center;
+  justify-content: center;
+`;
+// white yerine colors.darkgrey olucak
+const CellLetter = styled.Text`
+  color: white;
+  font-weight:bold;
+  font-size:28;
+`;
+// white yerine colors.lightgrey olucak
+const Screen = styled.View`
+  flex: 1;
+  background: ${colors.black};
+  align-items: center;
+  padding-top: 40px;
+`;
+const HeaderTitle = styled.Text`
+  color: ${colors.lightgrey};
+  font-size: 32px;
+  font-weight: bold;
+  letter-spacing: 6px;
+`;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  map: {},
 });
